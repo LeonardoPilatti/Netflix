9fa50415065033784882c45f3f0ee433db12c3e2
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-05-28 15:15:02 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Tmdb from './Tmdb';
 
 function App() {
+  const [movieList, setMovieList] = React.useState([]);
+
+  React.useEffect(() => {
+    const loadAll = async () => {
+      /// Pegando a lista Total:
+      let list = await Tmdb.getHomeList();
+      // console.log(list);
+      setMovieList(list);
+    };
+    loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <p>Este é um teste</p>
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>{item.title}</div>
+        ))}
+      </section>
     </div>
   );
 }
